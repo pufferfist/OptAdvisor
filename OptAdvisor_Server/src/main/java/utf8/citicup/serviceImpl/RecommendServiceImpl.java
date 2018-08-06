148ae71da1a8cc0f928bddd2f4e07395d3906663
BeiMaGang <161220137@smail.nju.edu.cn>
2018-08-06 17:30:42 +0800

    0

---
+++ OptAdvisor_Server/src/main/java/utf8/citicup/serviceImpl/RecommendServiceImpl.java
@@ -0,0 +1,24 @@
+package utf8.citicup.serviceImpl;
+
+import utf8.citicup.domain.entity.Option;
+import utf8.citicup.domain.entity.RecommendOption1;
+import utf8.citicup.domain.entity.RecommendOption2;
+import utf8.citicup.domain.entity.RecommendOption3;
+import utf8.citicup.service.RecommendService;
+
+public class RecommendServiceImpl implements RecommendService {
+    @Override
+    public RecommendOption1 recommendPortfolio(double M0, double k, int T, char combination, double p1, double p2, double sigma1, double sigma2) {
+        return null;
+    }
+
+    @Override
+    public RecommendOption2 heging(int N0, double a, double s_exp, int T) {
+        return null;
+    }
+
+    @Override
+    public RecommendOption3 customPortfolio(Option[] list) {
+        return null;
+    }
+}
